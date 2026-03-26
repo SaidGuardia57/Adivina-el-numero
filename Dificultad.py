650a27d535ec863827132d4ceddbfeb5f90d9742
Gian <gian.pitti@uip.edu.pa>
2026-03-25 23:10:18 -0500

    se funcion de dificultad actualizada

--- Dificultad.py
+++ Dificultad.py
@@ -1,17 +1,20 @@
 def elegir_dificultad():
-    print("Selecciona la dificultad:")
-    print("1. Fácil (1-10)")
-    print("2. Medio (1-20)")
-    print("3. Difícil (1-50)")
+    while True:
+        print("Selecciona la dificultad:")
+        print("1. Fácil (1-10)")
+        print("2. Medio (1-20)")
+        print("3. Difícil (1-50)")
 
-    opcion = input("Elige una opción (1/2/3): ")
+        opcion = input("Elige una opción (1/2/3): ")
 
-    if opcion == "1":
-        return 1, 10
-    elif opcion == "2":
-        return 1, 20
-    elif opcion == "3":
-        return 1, 50
-    else:
-        print("Opción inválida, se usará dificultad media.")
-        return 1, 20
+        if opcion == "1":
+            print("Elegiste Fácil\n")
+            return 1, 10
+        elif opcion == "2":
+            print("Elegiste Medio\n")
+            return 1, 20
+        elif opcion == "3":
+            print("Elegiste Difícil\n")
+            return 1, 50
+        else:
+            print("Opción inválida, intenta de nuevo.\n")

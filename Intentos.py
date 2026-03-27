6c2ea0c07b1c02da1ba3ad4e6396e521ec634a53
Maria-uip <maria.cedenov@uip.edu.pa>
2026-03-26 22:02:21 -0500

    Actualización

--- Intentos.py
+++ Intentos.py
@@ -1,5 +1,12 @@
-intentos = 5
+#intentos.py
+
+def manejar_intentos(numero_secreto, minimo, maximo):
+    intentos = 5
 
 while intentos > 0:
     numero = int(input("Adivina el número: "))
     intentos -= 1
+
+    print(f" Upss Perdiste, El número era {numero_secreto}\n")
+    return False
+# Versión final de Maria
